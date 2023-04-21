bd00c013c55954a4ac32f5dba6f717a0d1a70ac8
mrseahorse <131419526+mrseahorse@users.noreply.github.com>
2023-04-21 11:45:55 +0900

    added string example

--- Demo.py
+++ Demo.py
@@ -8,3 +8,31 @@ y=2
 
 if x==1 and y ==2:
     print("- mrseahorse")
+
+# Integers
+myint = 7
+print ("Integers are whole numbers") 
+print (myint)
+
+# Floating Point 
+print ("Floats are decimals")
+myfloat = 7.0
+print (myfloat)
+myfloat = float(7)
+print (myfloat)
+
+#Strings
+mystring = "Don't worry about apostrophes if using double quotes"
+print (mystring)
+
+#Simple Operators
+x = 3
+y = 5
+z = x + y
+print(z)
+
+elf = "Elf"
+dwarf = "Dwarven"
+mix = dwarf + " " + elf
+print (mix)
+
