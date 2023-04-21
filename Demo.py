ed8a8f45b60bf4b990d5f65a907ed37b0725e69d
mrseahorse <131419526+mrseahorse@users.noreply.github.com>
2023-04-21 18:15:00 +0900

    Done with Lists exercise Be careful, you forgot to add quotation marks to each name on the names list

--- Demo.py
+++ Demo.py
@@ -21,11 +21,11 @@ print (myfloat)
 myfloat = float(7)
 print (myfloat)
 
-#Strings
+# Strings
 mystring = "Don't worry about apostrophes if using double quotes"
 print (mystring)
 
-#Simple Operators
+# Simple Operators
 x = 3
 y = 5
 z = x + y
@@ -36,3 +36,47 @@ dwarf = "Dwarven"
 mix = dwarf + " " + elf
 print (mix)
 
+# Simultaneous Assigment
+x, y = 5, 6
+print (y,x)
+
+# Operators between numbers and strings is not supported
+# e.g. print (x + y + mix)
+
+
+# Exercise
+mystring = dwarf
+myfloat = 10.0
+myint = 20
+
+print ("String: %s" % mystring)
+print ("Integer: %d" % myint)
+print ("Float: %f" % myfloat)
+
+# Lists
+mylist = []
+mylist.append(1)
+mylist.append(2)
+mylist.append(3)
+print(mylist[0]) # prints 1
+print(mylist[1]) # prints 2
+print(mylist[2]) # prints 3
+
+# prints out 1,2,3
+for x in mylist:
+    print (x)
+
+# List Exercise
+numbers = []
+strings = []
+names = ["Jack", "Bear", "Rose"]
+
+numbers.append([1,2,3])
+strings.append("hello")
+strings.append("world")
+
+second_name = names[1]
+
+print(numbers)
+print(strings)
+print("The second name on the names list is %s" % second_name)
